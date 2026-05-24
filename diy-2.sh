fb4bb53a8a4322dc50afda014723cabd50f11a68
Donnach <70801913+Donnach@users.noreply.github.com>
2026-05-24 23:20:03 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -2,4 +2,4 @@
 
 # 添加包
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.11/g' package/base-files/files/bin/config_generate
